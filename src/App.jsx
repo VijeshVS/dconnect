c96e298596c9e59dc353b15d983dcb50ed71558d
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-10-09 22:08:18 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,66 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./config";
 import { ethers } from "ethers";
+import { Button, Flex } from "antd";
+import ConnectWalletModal from "./components/WalletConnectModal";
+import { Toaster, toast } from 'sonner'
 
 function App() {
-  const [contract, setContract] = useState(null);
+  const [isUserConnected, setIsUserConnected] = useState(false);
+  const [accounts, setAccounts] = useState();
 
   useEffect(() => {
-
-    async function getSettle(){
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      const signer = await provider.getSigner()
-      const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
-      setContract(contract);
-      console.log("done")
-    }
-
-    getSettle();
-    
+    window.ethereum
+      .request({
+        method: "eth_accounts",
+        params: [],
+      })
+      .then((res) => {
+        if (res.length == 0) {
+          alert("User is not connected");
+        } else {
+          setIsUserConnected(true);
+          setAccounts(res);
+        }
+      });
   }, []);
 
-  async function textChange(){
-    const tx = await contract.changeWelcome("snfn");
-    console.log(tx)
-  }
-
   return (
-    <div className="container">
-      <button onClick={textChange}>Call Smart Contract</button>
-    </div>
+    <main>
+      <nav className="shadow-md border-b-[0.5px] py-4 flex justify-between">
+        <div className="flex w-full justify-between px-4">
+          <h1 className="text-3xl font-bold">dConnect</h1>
+          <ConnectWalletModal isUserConnected={isUserConnected} />
+        </div>
+      </nav>
+      <Toaster />
+    </main>
   );
 }
 
 export default App;
+
+// const [contract, setContract] = useState(null);
+
+// useEffect(() => {
+//   async function getSettle(){
+//     const provider = new ethers.BrowserProvider(window.ethereum);
+//     const signer = await provider.getSigner();
+//     const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+//     setContract(contract);
+//   }
+
+//   getSettle();
+
+// }, []);
+
+// async function textChange(){
+//   const tx = await contract.changeWelcome("snfn");
+//   console.log(tx)
+// }
+
+// return (
+//   <div className="container">
+//     <button onClick={textChange}> Call Smart Contract </button>
+//   </div>
+// );
---
+++ src/components/WalletConnectModal.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
+import { Spin } from "antd";
+import { toast } from "sonner";
+
+const ConnectWalletModal = ({ isUserConnected }) => {
+  const [walletConnecting, setWalletConnecting] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  async function requestWalletConnection() {
+    setWalletConnecting(true);
+
+    try {
+      await window.ethereum.request({
+        method: "eth_requestAccounts",
+        params: [],
+      });
+      toast.success("Wallet connected successfully");
+    } catch (e) {
+      toast.error("Wallet access denied");
+    }
+    setWalletConnecting(false);
+    setIsModalOpen(false);
+  }
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <>
+      {isUserConnected ? (
+        <Button size="middle" disabled>
+          Wallet Connected
+        </Button>
+      ) : (
+        <Button onClick={showModal} type="primary">
+          Connect your wallet
+        </Button>
+      )}
+      <Modal
+        footer={<></>}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <div className="flex flex-col space-y-3">
+          <h1 className="text-xl font-semibold">Connect your wallet</h1>
+          <Button onClick={requestWalletConnection} className="w-fit">
+            Connect
+          </Button>
+          {walletConnecting ? <Spin /> : <></>}
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default ConnectWalletModal;
